cae216e9be3685f7b329f0afa5660cc4dd9ee67c
sanposhiho <handbomusic@gmail.com>
2020-10-20 17:49:33 +0900

    add: prd作ってみた

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule Nippol.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:dialyxir, "~> 0.4", only: [:dev]},
+      {:cowboy, "< 2.8.0", override: true}
     ]
   end
 
